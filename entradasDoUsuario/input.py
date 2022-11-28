814305fecc4e1d3f77bd40df1c095a7a30d9de12
Cibele Martins <Cibele.Martins@ibm.com>
2022-11-28 13:08:39 -0300

    🚧 feat: implementou exercício 2 sobre entradas do usuário

--- entradasDoUsuario/input.py
+++ entradasDoUsuario/input.py
@@ -2,11 +2,26 @@
 # ou pode receber mais de uma entrada; nesse caso deve ser
 # implementado em conjunto com o método split()
 
-receiveName = input("Digite o seu nome: ")
-print(f"O nome inserido foi: {receiveName}")
+# receiveName = input("Digite o seu nome: ")
+# print(f"O nome inserido foi: {receiveName}")
 
-num1, num2 = input("Digite dois números ").split()
 
-result = int(num1) // int(num2)
+# # exercício 1
+# num1, num2 = input("Digite dois números ").split()
 
-print(f"O resultado da divisão dos números foi: {result}")
+# result = int(num1) // int(num2)
+
+# print(f"O resultado da divisão dos números foi: {result}")
+
+# exercício 2
+
+dia = input("Digite o dia: ")
+mes = input("Digite o mes: ")
+ano = input("Digite o ano: ")
+hora = input("Digite a hora: ")
+minuto = input("Digite o minuto: ")
+
+print(f"A data e horário inseridos foram: {dia}/{mes}/{ano} e {hora}:{minuto}")
+
+
+
