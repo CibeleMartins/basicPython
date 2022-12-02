e87aaaf874c41de997cfcf9e5ba7b8e67bdb2f56
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-02 14:42:34 -0300

    🚧 feat: Implementou exercício 3 com while

--- ifWhileFor/while.py
+++ ifWhileFor/while.py
@@ -18,16 +18,30 @@ soma = 0
 """ 2. Crie um programa que receba 5 números, realiza a soma destes números, 
 mas caso um destes números seja negativo não considere ele na soma"""
 
-while contador < 5:
+# while contador < 5:
 
+#     contador += 1
+
+#     num__recebido = float(input("Digite um número: "))
+    
+#     if num__recebido < 0:
+
+#         soma -= num__recebido
+#     else :
+#         soma += num__recebido
+
+# print(f"O resultado é: {soma}")
+
+"""Crie um programa que receba como input uma string.
+Em seguida percorra a string e diga quantos espaços em branco essa string tem."""
+
+string = input("Digite uma palavra ou frase: ")
+quantidade_espaco = 0
+
+while (contador < len(string)):
+
+    if (string[contador] == " "):
+        quantidade_espaco += 1
     contador += 1
 
-    num__recebido = float(input("Digite um número: "))
-    
-    if num__recebido < 0:
-
-        soma -= num__recebido
-    else :
-        soma += num__recebido
-
-print(f"O resultado é: {soma}")
+print(f"O número de espaços é de: {quantidade_espaco}")
