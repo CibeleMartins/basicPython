91acd3ed97a25637acce659388ed7fcc2dc12694
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-02 15:29:24 -0300

    🚧 feat: implementou exercício 4 com laço for

--- ifWhileFor/for.py
+++ ifWhileFor/for.py
@@ -26,18 +26,25 @@ quantos caracteres ela tem, nao leve em conta caracteres de espaço."""
 
 """3. Crie um programa que leia uma quantidade arbitraria de textos e concatene eles numa string única."""
 
-quantidade_textos = int(input("Insira a quantidade de textos que vc quer concatenar: "))
-string_unica = ""
+# quantidade_textos = int(input("Insira a quantidade de textos que vc quer concatenar: "))
+# string_unica = ""
 
-for i in range(1,quantidade_textos+1):
+# for i in range(1,quantidade_textos+1):
 
-    texto = input("Digite uma palavra ou texto")
+#     texto = input("Digite uma palavra ou texto")
 
-    string_unica += texto
+#     string_unica += texto
 
-print(string_unica)
+# print(string_unica)
 
 
 """4. Crie um programa que printe a tabuada da divisão de um número lido por input."""
+
+numero_recebido = input("Digite um número: ")
+numero_recebido_formatado = int(numero_recebido)
+
+for i in range(1, numero_recebido_formatado+1):
+
+    print(f"{numero_recebido} / {i} = {numero_recebido_formatado / i}")
 
 """5. Crie um programa que percorra os números de 3 até 30 e diga se o número é primo ou não."""
