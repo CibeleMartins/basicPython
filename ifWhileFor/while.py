4b7eb672be1966adfeddb4a6b4c9b5cdb9b3411a
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-02 14:29:39 -0300

    🚧 feat: Implementou exercício 2 com while

--- ifWhileFor/while.py
+++ ifWhileFor/while.py
@@ -1,16 +1,33 @@
+"""1. Crie um programa que receba 5 números e retorne a média aritmética entre esses números"""
 
 contador = 0
 soma = 0
+
+# while contador < 5:
+
+#     contador += 1
+
+#     num_lido = input("Digite um número: ")
+#     num_float = float(num_lido)
+#     soma += num_float
+
+# media = soma / 5
+
+# print(f"A média aritmética é: {media}")
+
+""" 2. Crie um programa que receba 5 números, realiza a soma destes números, 
+mas caso um destes números seja negativo não considere ele na soma"""
 
 while contador < 5:
 
     contador += 1
 
-    num_lido = input("Digite um número: ")
-    num_float = float(num_lido)
-    soma += num_float
+    num__recebido = float(input("Digite um número: "))
+    
+    if num__recebido < 0:
 
-media = soma / 5
+        soma -= num__recebido
+    else :
+        soma += num__recebido
 
-print(f"A média aritmética é: {media}")
-
+print(f"O resultado é: {soma}")
