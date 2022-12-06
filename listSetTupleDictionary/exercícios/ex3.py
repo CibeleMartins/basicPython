7cb942b2ecdbed2594410bbca5bc7ff96a167c4f
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-06 15:04:00 -0300

    🚧 feat: implementou exercício 3 de listas

---
+++ listSetTupleDictionary/exercícios/ex3.py
@@ -0,0 +1,8 @@
+# Crie duas listas com os seguintes valores 30,4,43,52,65,-10 e 
+# 43,2,4,76,32,65,3. Faça a junção dessas listas, retirando os valores repetidos.
+
+lista1 = {30,4,43,52,65,-10}
+lista2= {43,2,4,76,32,65,3}
+uniao = lista1.union(lista2)
+
+print(uniao)
