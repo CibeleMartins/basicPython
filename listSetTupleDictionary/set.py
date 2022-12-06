5ab399c99c90be13568560fcf0a211a3b1054b51
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-06 13:22:14 -0300

    🚧 feat: implementou sets e alguns métodos

---
+++ listSetTupleDictionary/set.py
@@ -0,0 +1,14 @@
+setAcoes = {'Correr', 'Andar', 'Dirigir '}
+
+setNumeros = set({1,2,3,4})
+
+setNomes = {'João', 'Maria', 'Joana'}
+
+setA = {1,2,3,4}
+setB= {4,5,6,7}
+
+print(setAcoes, setNumeros)
+
+intersecao = setA.intersection(setB)
+diferenca = setA.difference(setB)
+print(intersecao, diferenca)
