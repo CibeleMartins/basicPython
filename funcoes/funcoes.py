6b2b98ae2afa2247bcf762f4bb6f30a0e22e25c6
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-14 15:37:34 -0300

    🚧 feat: implementou exercício de vogais

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -9,8 +9,8 @@ def e_negativo(numero):
     else:
         print(False)
 
-e_negativo(10)
-e_negativo(-60)
+# e_negativo(10)
+# e_negativo(-60)
 
 # 2 - Crie uma funcao que receba um array de numeros (int ou float)
 # e retorne sua soma
@@ -21,12 +21,21 @@ def somaNumerosArray(array_numeros):
 
     return soma
 
-print(somaNumerosArray([2,3,4]))
-print(somaNumerosArray([2,3,4,90,80,900]))
+# print(somaNumerosArray([2,3,4]))
+# print(somaNumerosArray([2,3,4,90,80,900]))
     
-
 #  3 - Crie uma funcao que receba uma string e que conte e retorne o numero de vogais desta string
 
-#  4 - Crie uma funcao que retorne o ultimo caractere de uma string recebida
+def contaVogais(string):
+
+    vogais = 'aeiou'
+    qtd_vogais = len([c for c in string.lower() if c in vogais])
+    print(qtd_vogais) 
+
+    print(f"A string tem {qtd_vogais} vogais.")
+    
+contaVogais('cibele')
+contaVogais('bbbabbabbb')
+#  4 - Crie uma funcaao que retorne o ultimo caractere de uma string recebida
 
 #  5 = Crie uma funcao que receba dois numeros e uam string dizendo se deve realizar a soma ou a subtracao dos numeros
