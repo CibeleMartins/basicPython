b2d312b5a103d721febe50a4d98d0d77bb668122
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-14 16:17:09 -0300

    💬 feat: adicionou enunciados dos exercícios

---
+++ funcoes/exerciciosII.py
@@ -0,0 +1,13 @@
+# 6 - Crie uma funcao que receba uma lista de elementos e um valor qualquer. Em seguida retorne um booleano
+# dixendo se o valor foi encontradoou nao na lista
+
+# 7 - Crie uma funcao que receba uma lista de elementos e um valor qualquer. Em seguida, retorne um booleano
+# dizendo se o valor foi encontrado ou nao e tambem a posicao onde foi encontrado
+
+#  8 - Crie uma funcao que recebe um numero arbitrario de parametros. Em seguida diga qual o tipo de cada parametro
+
+# 9 - Crie uma funcao que receba uma string, mas que possua um decorator para transforma-la em uma citacao, ou seja,
+# voce deve retornar a string entre aspas duplas, alem disso, transformar os carateres para minuscula usando a funcao lower()
+
+#  10 - Cria uma funcao recursiva que itere os numeros de 0 ate 10 e print o resultado de sua divisao inteira com onúmero 3.
+
