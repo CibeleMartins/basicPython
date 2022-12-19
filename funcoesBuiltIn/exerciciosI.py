787a22195f480eafaeab82d5b449542edcff7373
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-19 14:55:57 -0300

    💬 feat: Adicionou enunciado dos exercícios

---
+++ funcoesBuiltIn/exerciciosI.py
@@ -0,0 +1,12 @@
+# 1 - Crie uma função que retorne a subtração de dois elementos, mas que considere o valor absoluto destes valores.
+
+# 2 -  Sem usar "if`s", crie uma função que receba dois números e retorne a soma dos mesmos, mas o valor retornado não pode passar 
+# de 10000 e deve ser sempre maior que 0.
+
+# 3 - (DESAFIO) Crie uma função que receba argumentos de tamanho arbitrário. Todos serão números.
+# Em seguida retorne o menor número entre todos os recebidos
+
+# 4 - Crie uma funão que calcule a formula de Bhaskara, encontrado o X. 
+# Os coeficientes a, b e c devem ser lidos por input.
+
+#  5 - Crie uma função que receba uma string, e para cada letra minúscula a transforme para maiúscula e vice e versa.
