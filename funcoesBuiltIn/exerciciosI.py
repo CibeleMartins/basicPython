6962d0913a392c7e326819b8cd76fb7e3c5b0dca
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-19 16:17:30 -0300

    🚧 feat: implementou exercício 4 com o uso de math.sqrt() e pow()

--- funcoesBuiltIn/exerciciosI.py
+++ funcoesBuiltIn/exerciciosI.py
@@ -22,7 +22,7 @@ def soma(num1, num2):
 
     return soma
 
-print(soma(6000, 5000))
+# print(soma(6000, 5000))
 
 # 3 - (DESAFIO) Crie uma função que receba argumentos de tamanho arbitrário. Todos serão números.
 # Em seguida retorne o menor número entre todos os recebidos.
@@ -31,8 +31,27 @@ def menorNumero(*args):
 
     return print(min(args))
 
-menorNumero(1,3,4,5,6)
+# menorNumero(1,3,4,5,6)
+menorNumero(10,20,30,100000, -2)
+
 # 4 - Crie uma funão que calcule a formula de Bhaskara, encontrado o X. 
 # Os coeficientes a, b e c devem ser lidos por input.
+
+def calculaBhaskara():
+
+    titulo = "Vamos calcular a fórmula de bhaskara!"
+    titulo_centralizado = titulo.center(100, "-").upper()
+    print(titulo_centralizado)
+
+    a, b, c = map(int, input("Digite o valor de A, B e C:  ").split())
+
+    delta = pow(b,2) - 4 * a * c
+
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+
+    return print(x1, x2)
+    
+# calculaBhaskara()
 
 #  5 - Crie uma função que receba uma string, e para cada letra minúscula a transforme para maiúscula e vice e versa.
